5dedfc13748c57a74e3d9600eb8916985adf7d56
chen <1332093219@qq.com>
2020-03-13 01:33:42 +0800

    second

--- android/src/main/java/com/chen/media_notification/NotificationUtil.java
+++ android/src/main/java/com/chen/media_notification/NotificationUtil.java
@@ -128,7 +128,7 @@ public class NotificationUtil extends BroadcastReceiver {
             mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
             notificationManager.createNotificationChannel(mChannel);
             notification = new NotificationCompat.Builder(mContext, id)
-                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setSmallIcon(R.mipmap.defaults)
                     .setWhen(System.currentTimeMillis())
                     .setContentIntent(getDefaultIntent(Notification.FLAG_ONGOING_EVENT))
                     .setCustomBigContentView(getContentView(true))
@@ -141,7 +141,7 @@ public class NotificationUtil extends BroadcastReceiver {
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             notification = new NotificationCompat.Builder(mContext, id)
                     .setWhen(System.currentTimeMillis())
-                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setSmallIcon(R.mipmap.defaults)
                     .setContentIntent(getDefaultIntent(Notification.FLAG_ONGOING_EVENT))
                     .setCustomBigContentView(getContentView(true))
                     .setCustomContentView(getContentView(false))
@@ -152,7 +152,7 @@ public class NotificationUtil extends BroadcastReceiver {
         } else {
             notification = new NotificationCompat.Builder(mContext, id)
                     .setWhen(System.currentTimeMillis())
-                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setSmallIcon(R.mipmap.defaults)
                     .setContentIntent(getDefaultIntent(Notification.FLAG_ONGOING_EVENT))
                     .setContent(getContentView(false))
                     .setPriority(NotificationCompat.PRIORITY_HIGH)
